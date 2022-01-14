1a2823d429f82cc2982247dbf49c56a7996c66c9
Marcos Espinoza <meicoder7@gmail.com>
2022-01-14 13:00:44 -0400

    First Commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,16 @@
+export const getGifs = async (category) => {
+    const url = `http://api.giphy.com/v1/gifs/search?api_key=6JDbeJDfpUBL4SF7E85JL6pkZVCqzKc5&q=${encodeURI(
+        category
+    )}&limit=10`;
+    const respuesta = await fetch(url);
+    const { data } = await respuesta.json();
+
+    const gifs = data.map((img) => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images.downsized_medium.url
+        };
+    });
+    return gifs;
+};
